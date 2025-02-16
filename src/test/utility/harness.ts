96fdfafa28b73b633e4191d7c92306eb9b414be3
Rhys van der Waerden <rhys.vdw@gmail.com>
2025-02-16 20:48:40 +1100

    Change test message

--- src/test/utility/harness.ts
+++ src/test/utility/harness.ts
@@ -3,7 +3,7 @@ import { members } from "../..";
 
 export function testMembers(name: string, input: string, expected: string) {
   test(name, (t) => {
-    t.isEqual(members(input), expected);
+    t.isEqual(members(input), expected, "Has correct output");
     t.end();
   });
 }
