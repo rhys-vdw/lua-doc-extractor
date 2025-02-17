ebb77dfb2dfea2fbd793586a35470507451ead4e
Rhys van der Waerden <rhys.vdw@gmail.com>
2025-02-18 01:56:23 +1100

    Pull out RawComment

--- src/lexer.ts
+++ src/lexer.ts
@@ -1,4 +1,5 @@
-import moo from "moo";
+import moo, { Token } from "moo";
+import { Position } from "./source";
 
 const lexer = moo.states({
   code: {
@@ -7,7 +8,7 @@ const lexer = moo.states({
     space: { match: /\s+/, lineBreaks: true },
   },
   blockComment: {
-    newline: "\n",
+    newline: { match: "\n", lineBreaks: true },
     indent: /^\s+\*(?!\/)/,
     blockCommentEnd: { match: "*/", pop: 1 },
     word: { match: /[^\s$]+/ },
@@ -15,15 +16,47 @@ const lexer = moo.states({
   },
 });
 
-const c = `/*** Changes the value of the (one-sided) alliance between: firstAllyTeamID -> secondAllyTeamID.
+const c = `
+/*** Changes the value of the (one-sided) alliance between: firstAllyTeamID -> secondAllyTeamID.
  *
  * @function Spring.SetAlly
  * @param firstAllyTeamID integer
  * @param secondAllyTeamID integer
- * @param ally boolean x * x
+ * @param ally boolean
  * @return nil
  */
 int LuaSyncedCtrl::SetAlly(lua_State* L)
+{
+	const int firstAllyTeamID = luaL_checkint(L, 1);
+	const int secondAllyTeamID = luaL_checkint(L, 2);
+
+	if (!teamHandler.IsValidAllyTeam(firstAllyTeamID))
+		return 0;
+	if (!teamHandler.IsValidAllyTeam(secondAllyTeamID))
+		return 0;
+
+	teamHandler.SetAlly(firstAllyTeamID, secondAllyTeamID, luaL_checkboolean(L, 3));
+	return 0;
+}
+
+
+/*** Changes the start box position of an allyTeam.
+ *
+ * @function Spring.SetAllyTeamStartBox
+ * @param allyTeamID integer
+ * @param xMin number left start box boundary (elmos)
+ * @param zMin number top start box boundary (elmos)
+ * @param xMax number right start box boundary (elmos)
+ * @param zMax number bottom start box boundary (elmos)
+ * @return nil
+ */
+int LuaSyncedCtrl::SetAllyTeamStartBox(lua_State* L)
+{
+	const unsigned int allyTeamID = luaL_checkint(L, 1);
+	const float xMin = luaL_checkfloat(L, 2);
+	const float zMin = luaL_checkfloat(L, 3);
+	const float xMax = luaL_checkfloat(L, 4);
+	const float zMax = luaL_checkfloat(L, 5);
 `;
 
 const b = `blah blah /*** boo */ blaz`;
@@ -38,24 +71,65 @@ function detail(s: string) {
   console.log(result.join("\n"));
 }
 
+export interface RawComment {
+  start: Position;
+  end: Position;
+  text: string;
+}
+
 function comments(s: string) {
-  const result = [];
+  const result = [] as RawComment[];
+  let current = null as { text: string[]; start: Position } | null;
+
   lexer.reset(s);
-  let block = false;
   for (const entry of lexer) {
+    // Check for end of comment.
     if (entry.type === "blockCommentEnd") {
-      block = false;
-    }
-    if (block) {
-      if (entry.type !== "indent") {
-        result.push(entry.text);
+      if (current === null) {
+        console.error(
+          `Encountered "${entry.text}" when not in a comment block: ${entry.line}:${entry.col}`
+        );
+      } else {
+        console.log(entry);
+        result.push({
+          text: current.text.join(""),
+          start: current.start,
+          end: {
+            lineNumber: entry.line,
+            columnNumber: entry.col + entry.text.length - 1,
+          },
+        });
+        current = null;
       }
     }
+
+    // Accumulate comment body.
+    if (current !== null && entry.type !== "indent") {
+      current.text.push(entry.text);
+    }
+
+    // Start a new comment.
     if (entry.type === "blockCommentStart") {
-      block = true;
+      if (current !== null) {
+        console.error(
+          `Encountered "${entry.text}" when in a comment block: ${entry.line}:${entry.col}`
+        );
+      } else {
+        current = {
+          text: [],
+          start: { lineNumber: entry.line, columnNumber: entry.col },
+        };
+      }
     }
   }
-  console.log(result.join(""));
+  console.log(
+    result
+      .map(
+        (r) =>
+          `-------- ${r.start.lineNumber}:${r.start.columnNumber}-${r.end.lineNumber}:${r.end.columnNumber}\n${r.text}`
+      )
+      .join("")
+  );
 }
 
 function array(s: string) {
