b7b2527ced5711f1ddadc5d6d79d40dcb1e7b380
Rhys van der Waerden <rhys.vdw@gmail.com>
2025-02-20 20:52:28 +1100

    Add more tests to harness

--- src/test/utility/harness.ts
+++ src/test/utility/harness.ts
@@ -1,5 +1,7 @@
 import test from "tape";
 import { members } from "../..";
+import { getComments } from "../../comment";
+import { docLexer } from "../../docLexer";
 
 export function testMembers(
   name: string,
@@ -9,19 +11,34 @@ export function testMembers(
 ) {
   const testFn = only ? test.only : test;
   testFn(name, (t) => {
-    const [actual, err] = members(input, "PATH");
-    if (err != null) {
-      t.error(err);
-    } else {
-      t.isEqual(actual, expected, "Has correct output");
-    }
-    t.end();
+    const [comments, error] = getComments(input);
+    t.error(error, "getComments succeeds");
 
-    if (only) {
-      console.log("---EXPECTED---");
-      console.log(expected);
-      console.log("---ACTUAL---");
-      console.log(actual);
+    comments?.forEach(({ text }) => {
+      t.doesNotThrow(() => {
+        docLexer.reset(text);
+        Array.from(docLexer);
+      }, `Successfully lexes comment: '${text.substring(0, 20)}...'`);
+    });
+
+    const [luaResult, err] = members(input, "PATH");
+
+    if (err != null) {
+      t.error(err, "members succeeds");
+    } else {
+      t.equal(luaResult.docErrors.length, 0, "docErrors is empty");
+      luaResult.docErrors.forEach((e) => t.error(e, "docError"));
+
+      t.isEqual(luaResult.lua, expected, "members has correct output");
+
+      if (only) {
+        console.log("---EXPECTED---");
+        console.log(expected);
+        console.log("---ACTUAL---");
+        console.log(luaResult.lua);
+      }
     }
+
+    t.end();
   });
 }
