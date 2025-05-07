3f3a78675a27913218263cc53997b010bbea91a3
Rhys van der Waerden <rhys.vdw@gmail.com>
2025-05-08 00:42:29 +1000

    Add failing test for whitespace

---
+++ src/test/whitespace.test.ts
@@ -0,0 +1,22 @@
+import dedent from "dedent-js";
+import { testInput } from "./utility/harness";
+
+testInput(
+  "Ignores trailing whitespace",
+  [
+    "/***",
+    " * @class Foo",
+    " * @field disabled boolean",
+    " * @field name string ",
+    " * @field value string ",
+    " * @field submitted boolean",
+    " */",
+  ].join("\n"),
+  dedent`
+    ---@class Foo
+    ---@field disabled boolean
+    ---@field name string
+    ---@field value string
+    ---@field submitted boolean
+  `
+);
